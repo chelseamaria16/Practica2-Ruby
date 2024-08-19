9e0b80528bdfc6c52c93677c20f059cb9bc12ae9
Chelsy <chelsea.moraga@est.unanleon.edu.ni>
2024-08-19 17:09:48 -0600

    Guardado

---
+++ ejercicio4.rb
@@ -0,0 +1,13 @@
+semana = ["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"]
+puts "Array completo"
+puts semana.to_s
+puts "\nMétodo POP"
+puts semana.pop
+puts "\nMétodo length"
+puts semana.length
+puts "\nÚltimo dato"
+puts semana.last
+puts "\nMétodo PUSH"
+puts semana.push "final"
+puts "\nTamaño nuevo"
+puts semana.length
